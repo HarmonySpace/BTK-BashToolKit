fa325c3d0ec276b072d729954db3e5b1e0489819
HarmonySpace <jchavaspace@gmail.com>
2024-01-19 10:12:44 -0600

    fix a mesage

--- scripts/navegation/navegation.sh
+++ scripts/navegation/navegation.sh
@@ -1,5 +1,5 @@
 remove_line_in $temp1 "option_selected"
-print_montse2 "¿Qué deseas hacer con \"directorio\"?"
+print_montse2 "¿Qué deseas hacer con \"directory\"?"
 add_in $temp1 "$(echo option_selected = $(choose_one "  copiar directorio"))"
 if_null "$(search_in $temp1 "option_selected" = 2)"
 print_user "$(search_in $temp1 "option_selected" = 2)"
