932e841803334d1a9e2afb7feb9f29e04dea3e6c
HarmonySpace <jchavaspace@gmail.com>
2024-01-18 22:56:59 -0600

    fix prints_b to take 80% of actual screen

--- services/prints.sh
+++ services/prints.sh
@@ -11,9 +11,21 @@ function print_message() {
   gum style --foreground $FR --align left "${1}"
   return 0
 }
+function print_message_b() {
+  mwidth=$(( $(tput cols) / 2 )) && mwidth=$(( $mwidth / 2 )) && mwidth=$(( $mwidth * 3 ))
+  gum style --foreground $FR --align left --border normal --border-foreground $FR --width $mwidth "${1}"
+  echo ""
+  return 0
+}
 ## print a body file
 function print_file() {
   gum style --foreground $FT --align left "${1}"
+  return 0
+}
+function print_file_b() {
+  mwidth=$(( $(tput cols) / 2 )) && mwidth=$(( $mwidth / 2 )) && mwidth=$(( $mwidth * 3 ))
+  gum style --foreground $FT --align left --border normal --border-foreground $FT --width $mwidth "${1}"
+  echo ""
   return 0
 }
 ## print a key message
@@ -21,9 +33,21 @@ function print_key (){
   gum style --foreground $CL1 --align left "${1}"
   return 0
 }
+function print_key_b (){
+  mwidth=$(( $(tput cols) / 2 )) && mwidth=$(( $mwidth / 2 )) && mwidth=$(( $mwidth * 3 ))
+  gum style --foreground $CL1 --align left --border normal --border-foreground $CL1 --width $mwidth "${1}"
+  echo ""
+  return 0
+}
 ## print a key second message
 function print_key2 (){
   gum style --foreground $CL2 --align left "${1}"
+  return 0
+}
+function print_key2_b (){
+  mwidth=$(( $(tput cols) / 2 )) && mwidth=$(( $mwidth / 2 )) && mwidth=$(( $mwidth * 3 ))
+  gum style --foreground $CL2 --align left --border normal --border-foreground $CL2 --width $mwidth "${1}"
+  echo ""
   return 0
 }
 ## print a key message
@@ -31,14 +55,34 @@ function print_user (){
   gum style --foreground $UFR --align left "${1}"
   return 0
 }
+function print_user_b (){
+  mwidth=$(( $(tput cols) / 2 )) && mwidth=$(( $mwidth / 2 )) && mwidth=$(( $mwidth * 3 ))
+  echo $mwidth
+  gum style --foreground $UFR --align left --border normal --border-foreground $UFR --width $mwidth "${1}"
+  echo ""
+  return 0
+}
 ## print a warning message
 function print_warning (){
   gum style --foreground $WR --align left "${1}"
   return 0
 }
+function print_warning_b (){
+  mwidth=$(( $(tput cols) / 2 )) && mwidth=$(( $mwidth / 2 )) && mwidth=$(( $mwidth * 3 ))
+  gum style --foreground $WR --align left --border normal --border-foreground $WR --width $mwidth "${1}"
+  echo ""
+  return 0
+}
 ## print a error message
 function print_error (){
   gum style --foreground $ERR --align left "${1}"
+  return 0
+}
+function print_error_b (){
+  mwidth=$(( $(tput cols) / 2 )) && mwidth=$(( $mwidth / 2 )) && mwidth=$(( $mwidth * 3 ))
+  mwidth=$(( $(tput cols) / 2 ))
+  gum style --foreground $ERR --align left --border normal --border-foreground $ERR --width $mwidth "${1}"
+  echo ""
   return 0
 }
 ## print a custom message
@@ -52,20 +96,17 @@ function print_montse (){
   mwidth=$(( $(tput cols) / 2 ))
   gum join "$(print_custom $CL2 $CL2 normal center 0 "0 1" "=＾● ⋏ ●＾=")" "$(print_custom $CL2 $CL2 normal left "0 3" "0 1" "${1}" 0 "$mwidth")"
   echo ""
-  echo ""
 }
 function print_montse2 (){
   clear
   mwidth=$(( $(tput cols) / 2 ))
   gum join "$(print_custom $CL2 $CL2 normal center 0 "0 1" "= ^・ ⋏ ・^ =")" "$(print_custom $CL2 $CL2 normal left "0 3" "0 1" "${1}" 0 "$mwidth")"
   echo ""
-  echo ""
 }
 function print_montseHappy (){
   clear
   mwidth=$(( $(tput cols) / 2 ))
   gum join "$(print_custom $CL2 $CL2 normal center 0 "0 1" "= ^- ⋏ -^ =")" "$(print_custom $CL2 $CL2 normal left "0 3" "0 1" "${1}" 0 "$mwidth")"
-  echo ""
   echo ""
 }
 function print_montseSad (){
