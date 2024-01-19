0d26d5af1b5cd83e1b97ffef5426dc16fedbc814
HarmonySpace <jchavaspace@gmail.com>
2024-01-19 00:23:57 -0600

    improve montse scripts functions

--- scripts/montse.sh
+++ scripts/montse.sh
@@ -14,14 +14,7 @@ print_montse "¿Cómo te ayudo?"
 put_in $temp1 "$(echo option_selected = $(choose_one "  directory" "  github" "  archivos"))"
 if_null "$(search_in $temp1 "option_selected" = 2)"
 if [[ $(search_in $temp1 "option_selected" = 2) =~ "directory" ]]; then
-  remove_line_in $temp1 "option_selected"
-  print_montse2 "¿Qué deseas hacer con \"directorio\"?"
-  add_in $temp1 "$(echo option_selected = $(choose_one "  copiar directorio"))"
-  if_null "$(search_in $temp1 "option_selected" = 2)"
-  print_user "$(search_in $temp1 "option_selected" = 2)"
-  if [[ $(search_in $temp1 "option_selected" = 2) =~ "copiar directorio" ]]; then
-    source "$SCRIPT_DIR/../scripts/navegation/copy_dir.sh"
-  fi
+  source "$SCRIPT_DIR/../scripts/navegation/navegation.sh"
 elif [[ $(search_in $temp1 "option_selected" = 2) =~ "github" ]]; then
   remove_line_in $temp1 "option_selected"
   print_message "Opción para \"github\""
---
+++ scripts/navegation/navegation.sh
@@ -0,0 +1,8 @@
+remove_line_in $temp1 "option_selected"
+print_montse2 "¿Qué deseas hacer con \"directorio\"?"
+add_in $temp1 "$(echo option_selected = $(choose_one "  copiar directorio"))"
+if_null "$(search_in $temp1 "option_selected" = 2)"
+print_user "$(search_in $temp1 "option_selected" = 2)"
+if [[ $(search_in $temp1 "option_selected" = 2) =~ "copiar directorio" ]]; then
+  source "$SCRIPT_DIR/../scripts/navegation/copy_dir.sh"
+fi
