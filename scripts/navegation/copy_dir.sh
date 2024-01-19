5dd3558a1064c00987ad96e73d88d395bff64b8f
HarmonySpace <jchavaspace@gmail.com>
2024-01-19 09:46:45 -0600

    update copy-dir message prints

--- scripts/navegation/copy_dir.sh
+++ scripts/navegation/copy_dir.sh
@@ -1,6 +1,7 @@
 add_in $temp1 "$(echo actual_dir = $(pwd))"
 if_null "$(search_in $temp1 "actual_dir" = 2)"
-print_montseHappy "Dirección actual $(search_in $temp1 "actual_dir" = 2)"
+print_montseHappy "Dirección actual"
+print_message_b "$(search_in $temp1 "actual_dir" = 2)"
 search_in $temp1 "actual_dir" = 2 | xclip -selection clipboard
 if [[ "$TERM" == *"kitty"* ]]; then
   search_in $temp1 "actual_dir" = 2 | kitty +kitten clipboard
