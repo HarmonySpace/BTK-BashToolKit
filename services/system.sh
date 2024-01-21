348d3999598a56ac24fab36a0e69c4b96ffdde8b
HarmonySpace <jchavaspace@gmail.com>
2024-01-21 06:16:31 -0600

    add wait_enter function

--- services/system.sh
+++ services/system.sh
@@ -2,9 +2,8 @@
 
 # tester
 ## try carch structure
-function try_catch (){
-  if [[ "$?" = 0 ]]
-  then
+function try_catch() {
+  if [[ "$?" = 0 ]]; then
     log_debug "${1} successfuly"
     return 0
   else
@@ -15,7 +14,7 @@ function try_catch (){
   fi
 }
 ## if null
-function if_null () {
+function if_null() {
   log_debug "checking if argument is null"
   if [[ "${1}" = "" ]]; then
     clear
@@ -27,9 +26,13 @@ function if_null () {
     log_debug "argument is not null"
   fi
 }
+function wait_enter() {
+  echo -n "Presiona ENTER para continuar"
+  read -p " "
+}
 # system
 ## take a actual date
-function take_date (){
+function take_date() {
   export DATE=$(date +"%T")
   export HOUR=$(date +"%D")
 }
