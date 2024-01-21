c925920880c1ffe73725c50f58d430fe33bc5a6e
HarmonySpace <jchavaspace@gmail.com>
2024-01-20 21:14:38 -0600

    Holaaaaaaa

---
+++ scripts/gitcli/git_push.sh
@@ -0,0 +1,38 @@
+print_montse "Añadir archivos al git push"
+put_in $temp1 "$(echo files_to_add = $(choose_one "todos" "uno"))"
+if_null $(search_in $temp1 "files_to_add" = 2)
+print_montse2 "Archivos a añadir"
+print_user_b "$(search_in $temp1 "files_to_add" = 2)"
+if [[ $(search_in $temp1 "files_to_add" = 2) =~ "todos" ]]; then
+  add_in $temp1 "$(echo files_selected = .)"
+  if_null $(search_in $temp1 "files_selected" = 2)
+elif [[ $(search_in $temp1 "files_to_add" = 2) =~ "uno" ]]; then
+  temp_gu=$(create_temp)
+  try_catch "create a temp file"
+  add_in $temp1 "$(echo continue = yes)"
+  while [ "$(search_in $temp1 "continue" = 2)" = "yes" ]; do
+    add_in $temp_gu "$(file_browser)"
+    print_montseHappy "Archivos seleccionados"
+    print_user_b "$(cat $temp_gu)"
+    remove_line_in $temp1 "continue"
+    add_in $temp1 "$(echo continue = $(confirm_yn "¿Deseas seleccionar otro archivo?"))"
+    if_null $(search_in $temp1 "continue" = 2)
+    remove_line_in $temp1 "files_selected"
+    list_in $temp1 "/" $temp_gu 1 ' ' "files_selected"
+    if_null $(search_in $temp1 "files_selected" = 2)
+  done
+fi
+#spin_command "git add" "$(git add $(search_list_in "files_selected" $temp1 =)) && $(try_catch "git add")"
+print_montse "Commit para el push"
+add_in $temp1 "$(echo git_commit = $(input_text "Ingresa el commiit acá"))"
+if_null $(search_in $temp1 "git_commit" = 2)
+print_montseHappy " Commit obtenido"
+print_user_b "$(search_in $temp1 "git_commit" = 2)"
+#spin_command "git commit" "$(git commit -m "$(search_in $temp1 "git_commit" = 2)")"
+add_in $temp1 "$(echo actual_account = $(git config --get user.name))"
+if_null $(search_in $temp1 "actual_account" = 2)
+credentials "$(search_in $temp1 "actual_account" = 2)"
+#git push
+try_catch "git push"
+delete_temp
+print_key "$SEC3"
--- scripts/gitcli/gitcli.sh
+++ scripts/gitcli/gitcli.sh
@@ -1,6 +1,6 @@
 remove_line_in $temp1 "option_selected"
 print_montse2 "¿Qué deseas hacer con \"github\"?"
-add_in $temp1 "$(echo option_selected = $(choose_one "󰛐   show credentials" "  git push"))"
+add_in $temp1 "$(echo option_selected = $(choose_one "󰛐   show credentials" "   git push"))"
 if_null "$(search_in $temp1 "option_selected" = 2)"
 if [[ $(search_in $temp1 "option_selected" = 2) =~ "show credentials" ]]; then
   source "$SCRIPT_DIR/../scripts/gitcli/show_credentials.sh"
