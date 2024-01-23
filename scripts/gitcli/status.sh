87e29a45609e3c94f15d0bf19429a1368dd2ebf6
HarmonySpace <jchavaspace@gmail.com>
2024-01-23 11:49:27 -0600

    update services (confirm and git)

--- scripts/gitcli/status.sh
+++ scripts/gitcli/status.sh
@@ -1,3 +1,4 @@
 print_montse "Status de la rama actual"
 print_warning "Tu estatus de la rama actual"
 git status -s
+source "$SCRIPTS/../scripts/gitcli/show_branchs.sh"
--- scripts/gitcli/switch_branch.sh
+++ scripts/gitcli/switch_branch.sh
@@ -1,15 +1,7 @@
-source "$SCRIPTS/../scripts/gitcli/status.sh"
-remove_line_in $temp1 "continue"
-add_in $temp1 "$(echo continue = $(confirm_yn "¿Deseas continuar con el cambio de rama?"))"
-if_null $(search_in $temp1 "continue" = 2)
-if [[ "$(search_in $temp1 "continue" = 2)" = "no" ]]; then
-  print_montseSad "git switch cancelado"
-  exit 2
-fi
+confirm_git
 print_montse2 "Selecciona la rama"
 add_in $temp1 "$(echo go = $(git branch --list | grep -v "\*" | choose_one))"
 if_null $(search_in $temp1 "go" = 2)
-print_user "$(cat $temp1)"
 print_user "$(search_in $temp1 "go" = 2)"
 git switch "$(search_in $temp1 "go" = 2)"
 try_catch "change to other branch"
